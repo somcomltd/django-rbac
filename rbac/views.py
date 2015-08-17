235d0eb2d748d7bd7d7704d9088f24ac31335fa2
Mark Allen <mallen@somcom.com>
2015-08-17 10:42:07 +0100

    Added django REST framework serializer for Relationships.

--- rbac/views.py
+++ rbac/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from rbac.models import Relationship
+
+from rbac.serializers import RelationshipSerializer
+
+
+# API views
+
+class RelationshipViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows subscriptions to be viewed or edited.
+    """
+    queryset = Relationship.objects.all()
+    serializer_class = RelationshipSerializer
